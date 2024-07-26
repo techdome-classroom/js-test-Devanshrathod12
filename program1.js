16b825bb3ac99614faf89c65d2d36ba6626c72af
devansh rathod <rathoddevansh53@gmail.com>
2024-07-26 12:22:37 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,6 +1,17 @@
 const getTotalIsles = function (grid) {
 
- 
+    let isles = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                isles++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+
+    return isles;
 
 };
 
